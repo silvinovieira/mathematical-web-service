4f9354c55641f861cc0a597e4eea788399191a18
Silvino Vieira <silvinov@gmail.com>
2021-05-09 09:32:02 -0300

    test: calculate Ackermann

--- tests/test_ackermann.py
+++ tests/test_ackermann.py
@@ -1,6 +1,7 @@
 import pytest
 
 from app import app
+from resources import Ackermann
 
 
 class TestAckermann:
@@ -27,3 +28,19 @@ class TestAckermann:
     def test_get_when_n_is_negative_return_bad_request(self, test_client):
         response = test_client.get('/api/ackermann?m=3&n=-2')
         assert response.status_code == 400
+
+    def test_calculate_ackermann_0_0(self):
+        res = Ackermann.calculate(0, 0)
+        assert res == 1
+
+    def test_calculate_ackermann_0_1(self):
+        res = Ackermann.calculate(0, 1)
+        assert res == 2
+
+    def test_calculate_ackermann_1_0(self):
+        res = Ackermann.calculate(1, 0)
+        assert res == 2
+
+    def test_calculate_ackermann_3_4(self):
+        res = Ackermann.calculate(3, 4)
+        assert res == 125
