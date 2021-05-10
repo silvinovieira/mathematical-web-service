7f3723f11768113956e11e59f8ef46f30780228d
Silvino Vieira <silvinov@gmail.com>
2021-05-10 10:10:41 -0300

    feat: log calculation time and result

--- resources/_base.py
+++ resources/_base.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import time
 
 from flask import Flask
 from flask_restful import Resource
@@ -16,13 +17,22 @@ class BaseMathResource(Resource):
 
     def get(self):
         query_args = self._request_parser.parse_args()
+        start_time = time.time()
+
         try:
             result = self.calculate(**query_args)
-            return {'result': result}
         except ValueError as err:
-            logger.error(f'[{self.__class__.__name__.upper()}-ERROR] {err}')
+            logger.error(f'[{self._get_class_name()}-ERROR] {err}')
             return {'message': str(err)}, 400
+
+        calc_time = time.time() - start_time
+        logger.info(f'[{self._get_class_name()}-CALC-TIME] Calculated in {calc_time}s')
+        logger.info(f'[{self._get_class_name()}-CALC-RES] The result is {result}')
+        return {'result': result}
 
     @abstractmethod
     def calculate(self, **kwargs):
         pass
+
+    def _get_class_name(self):
+        return self.__class__.__name__.upper()
