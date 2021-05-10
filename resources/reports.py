8a437099b392a7a7175e48a764dc567bc37fb59e
Silvino Vieira <silvinov@gmail.com>
2021-05-10 10:27:50 -0300

    feat: improve report
    Add total calls and total successful ones.

--- resources/reports.py
+++ resources/reports.py
@@ -4,8 +4,10 @@ from app import LOG_FILE
 def generate_report():
     with open(f'../{LOG_FILE}', 'r') as log_file:
         log_data = log_file.read()
+        calls = log_data.count('ARGS')
         errors = log_data.count('ERROR')
-    return f'Errors: {errors}'
+        successful = log_data.count('CALC-RES')
+    return f'Calls: {calls}, errors: {errors}, successful: {successful}'
 
 
 if __name__ == '__main__':
