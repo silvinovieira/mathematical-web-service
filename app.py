2e3fecbd173d8fa640def77d09c04571912ec996
Silvino Vieira <silvinov@gmail.com>
2021-05-10 10:56:36 -0300

    fix: error report

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ from resources.reports import LOG_FILE
 app = Flask(__name__)
 api = Api(app, prefix='/api')
 
-logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
+if len(logging.getLogger().handlers) > 0:
+    logging.getLogger().setLevel(logging.INFO)
+else:
+    logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
 
 api.add_resource(Ackermann, '/ackermann')
 api.add_resource(Factorial, '/factorial')
--- resources/reports.py
+++ resources/reports.py
@@ -5,7 +5,7 @@ def generate_report():
     with open(f'{LOG_FILE}', 'r') as log_file:
         log_data = log_file.read()
         calls = log_data.count('ARGS')
-        errors = log_data.count('ERROR')
+        errors = log_data.count('-ERROR')
         successful = log_data.count('CALC-RES')
     return f'Calls: {calls}, errors: {errors}, successful: {successful}'
 
