721194b2a22b43ad6f6c50aaeeb0137c13dfbe0e
Silvino Vieira <silvinov@gmail.com>
2021-05-10 11:17:44 -0300

    feat: show reports as table

--- resources/reports.py
+++ resources/reports.py
@@ -4,11 +4,17 @@ LOG_FILE = 'math-web-service.log'
 def generate_report():
     with open(f'{LOG_FILE}', 'r') as log_file:
         log_data = log_file.read()
-        calls = log_data.count('ARGS')
-        errors = log_data.count('-ERROR')
-        successful = log_data.count('CALC-RES')
-    return f'Calls: {calls}, errors: {errors}, successful: {successful}'
+        report = []
+        for function in ('Ackermann', 'Factorial', 'Fibonacci'):
+            report.append({
+                'function': function,
+                'calls': log_data.count(f'{function.upper()}-ARGS'),
+                'errors': log_data.count(f'{function.upper()}-ERROR'),
+                'successful': log_data.count(f'{function.upper()}-CALC-RES'),
+            })
+    return report
 
 
 if __name__ == '__main__':
-    print(generate_report())
+    from tabulate import tabulate
+    print(tabulate(generate_report(), headers='keys'))
