5d9ec16cc65423e7f0462e9b93d8a545c4f83cef
Silvino Vieira <silvinov@gmail.com>
2021-05-10 11:51:30 -0300

    refactor: rename variable

--- resources/fibonacci.py
+++ resources/fibonacci.py
@@ -8,12 +8,12 @@ class Fibonacci(BaseMathResource):
             raise ValueError('n must be a non-negative integer')
 
         if n in (0, 1):
-            fibonacci = n
+            result = n
         else:
             latest_numbers = (0, 1)
             for i in range(2, n + 1):
                 fibonacci_i = sum(latest_numbers)
                 latest_numbers = (latest_numbers[-1], fibonacci_i)
                 logger.debug(f'fibonacci({i}) = {fibonacci_i}')
-            fibonacci = latest_numbers[-1]
-        return fibonacci
+            result = latest_numbers[-1]
+        return result
